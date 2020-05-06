f1f6d60bb19bccd6f2ed257f3213cf060f5c28b7
Paul Yeh <pyeh.tw@icloud.com>
2020-05-06 15:39:51 +0200

    unlock step4 onwards

--- src/FinalProject_Camera.cpp
+++ src/FinalProject_Camera.cpp
@@ -168,9 +168,6 @@ int main(int argc, const char *argv[])
 
         cout << "#4 : CLUSTER LIDAR POINT CLOUD done" << endl;
 
-        // REMOVE THIS LINE BEFORE PROCEEDING WITH THE FINAL PROJECT
-        continue; // skips directly to the next image without processing what comes beneath
-
         /* DETECT IMAGE KEYPOINTS */
 
         // convert current image to grayscale
