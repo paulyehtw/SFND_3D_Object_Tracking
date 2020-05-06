af49d83e6830494de2707adb6584331710d0b3c6
Paul Yeh <pyeh.tw@icloud.com>
2020-05-06 15:24:14 +0200

    implement matchBoundingBoxes

--- src/camFusion_Student.cpp
+++ src/camFusion_Student.cpp
@@ -22,12 +22,13 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes,
     cv::Mat X(4, 1, cv::DataType<double>::type);
     cv::Mat Y(3, 1, cv::DataType<double>::type);
 
-    for (auto it1 = lidarPoints.begin(); it1 != lidarPoints.end(); ++it1)
+    for (auto itrLidar = lidarPoints.begin(); itrLidar != lidarPoints.end(); ++itrLidar)
     {
         // assemble vector for matrix-vector-multiplication
-        X.at<double>(0, 0) = it1->x;
-        X.at<double>(1, 0) = it1->y;
-        X.at<double>(2, 0) = it1->z;
+        // Set Lidar point onto the homogeneous coordinates
+        X.at<double>(0, 0) = itrLidar->x;
+        X.at<double>(1, 0) = itrLidar->y;
+        X.at<double>(2, 0) = itrLidar->z;
         X.at<double>(3, 0) = 1;
 
         // project Lidar point into camera
@@ -36,29 +37,31 @@ void clusterLidarWithROI(std::vector<BoundingBox> &boundingBoxes,
         pt.x = Y.at<double>(0, 0) / Y.at<double>(0, 2); // pixel coordinates
         pt.y = Y.at<double>(1, 0) / Y.at<double>(0, 2);
 
-        vector<vector<BoundingBox>::iterator> enclosingBoxes; // pointers to all bounding boxes which enclose the current Lidar point
-        for (vector<BoundingBox>::iterator it2 = boundingBoxes.begin(); it2 != boundingBoxes.end(); ++it2)
+        // pointers to all bounding boxes which enclose the current Lidar point
+        vector<vector<BoundingBox>::iterator> enclosingBoxes;
+        for (vector<BoundingBox>::iterator itrBox = boundingBoxes.begin(); itrBox != boundingBoxes.end(); ++itrBox)
         {
             // shrink current bounding box slightly to avoid having too many outlier points around the edges
             cv::Rect smallerBox;
-            smallerBox.x = (*it2).roi.x + shrinkFactor * (*it2).roi.width / 2.0;
-            smallerBox.y = (*it2).roi.y + shrinkFactor * (*it2).roi.height / 2.0;
-            smallerBox.width = (*it2).roi.width * (1 - shrinkFactor);
-            smallerBox.height = (*it2).roi.height * (1 - shrinkFactor);
+            smallerBox.x = (*itrBox).roi.x + shrinkFactor * (*itrBox).roi.width / 2.0;
+            smallerBox.y = (*itrBox).roi.y + shrinkFactor * (*itrBox).roi.height / 2.0;
+            smallerBox.width = (*itrBox).roi.width * (1 - shrinkFactor);
+            smallerBox.height = (*itrBox).roi.height * (1 - shrinkFactor);
 
             // check wether point is within current bounding box
             if (smallerBox.contains(pt))
             {
-                enclosingBoxes.push_back(it2);
+                enclosingBoxes.push_back(itrBox);
             }
 
         } // eof loop over all bounding boxes
 
-        // check wether point has been enclosed by one or by multiple boxes
+        // check whether the point has been enclosed by one or by multiple boxes
         if (enclosingBoxes.size() == 1)
         {
             // add Lidar point to bounding box
-            enclosingBoxes[0]->lidarPoints.push_back(*it1);
+            enclosingBoxes[0]->lidarPoints.push_back(*itrLidar);
+            boundingBoxes[0].lidarPoints.push_back(*itrLidar);
         }
 
     } // eof loop over all Lidar points
@@ -69,20 +72,20 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
     // create topview image
     cv::Mat topviewImg(imageSize, CV_8UC3, cv::Scalar(255, 255, 255));
 
-    for (auto it1 = boundingBoxes.begin(); it1 != boundingBoxes.end(); ++it1)
+    for (auto itrBox = boundingBoxes.begin(); itrBox != boundingBoxes.end(); ++itrBox)
     {
         // create randomized color for current 3D object
-        cv::RNG rng(it1->boxID);
+        cv::RNG rng(itrBox->boxID);
         cv::Scalar currColor = cv::Scalar(rng.uniform(0, 150), rng.uniform(0, 150), rng.uniform(0, 150));
 
         // plot Lidar points into top view image
         int top = 1e8, left = 1e8, bottom = 0.0, right = 0.0;
         float xwmin = 1e8, ywmin = 1e8, ywmax = -1e8;
-        for (auto it2 = it1->lidarPoints.begin(); it2 != it1->lidarPoints.end(); ++it2)
+        for (auto itrLidar = itrBox->lidarPoints.begin(); itrLidar != itrBox->lidarPoints.end(); ++itrLidar)
         {
             // world coordinates
-            float xw = (*it2).x; // world position in m with x facing forward from sensor
-            float yw = (*it2).y; // world position in m with y facing left from sensor
+            float xw = (*itrLidar).x; // world position in m with x facing forward from sensor
+            float yw = (*itrLidar).y; // world position in m with y facing left from sensor
             xwmin = xwmin < xw ? xwmin : xw;
             ywmin = ywmin < yw ? ywmin : yw;
             ywmax = ywmax > yw ? ywmax : yw;
@@ -106,10 +109,10 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
 
         // augment object with some key data
         char str1[200], str2[200];
-        sprintf(str1, "id=%d, #pts=%d", it1->boxID, (int)it1->lidarPoints.size());
-        putText(topviewImg, str1, cv::Point2f(left - 250, bottom + 50), cv::FONT_ITALIC, 2, currColor);
+        sprintf(str1, "id=%d, #pts=%d", itrBox->boxID, (int)itrBox->lidarPoints.size());
+        putText(topviewImg, str1, cv::Point2f(left - 250, bottom + 50), cv::FONT_ITALIC, 0.5, currColor);
         sprintf(str2, "xmin=%2.2f m, yw=%2.2f m", xwmin, ywmax - ywmin);
-        putText(topviewImg, str2, cv::Point2f(left - 250, bottom + 125), cv::FONT_ITALIC, 2, currColor);
+        putText(topviewImg, str2, cv::Point2f(left - 250, bottom + 125), cv::FONT_ITALIC, 0.5, currColor);
     }
 
     // plot distance markers
@@ -132,7 +135,7 @@ void show3DObjects(std::vector<BoundingBox> &boundingBoxes, cv::Size worldSize, 
     }
 }
 
-// associate a given bounding box with the keypoints it contains
+// associate a given bounding box with the keypoints boundingBoxPrev contains
 void clusterKptMatchesWithROI(BoundingBox &boundingBox,
                               std::vector<cv::KeyPoint> &kptsPrev,
                               std::vector<cv::KeyPoint> &kptsCurr,
@@ -165,5 +168,61 @@ void matchBoundingBoxes(std::vector<cv::DMatch> &matches,
                         DataFrame &prevFrame,
                         DataFrame &currFrame)
 {
-    // ...
+    // Loop over every bounding box in the previous frame
+    for (auto boundingBoxPrev : prevFrame.boundingBoxes)
+    {
+        // Create a container for storing matches that are enclosed by this bounding box in previous frame
+        vector<cv::DMatch> matchesEnclosedInPrev;
+
+        // Check how many keypoints each bounding box encloses
+        for (auto match : matches)
+        {
+            int prevKeyPointIdx = match.queryIdx;
+            cv::Point prevKeyPoint = prevFrame.keypoints.at(prevKeyPointIdx).pt;
+
+            if (boundingBoxPrev.roi.contains(prevKeyPoint))
+            {
+                matchesEnclosedInPrev.push_back(match);
+            }
+        }
+
+        // Create a map that stores the IDs of bounding box in current frame which also enclose the same matches
+        // as in previous frame.
+        std::multimap<int, int> sameMatchesCurr;
+
+        // Loop over each match that is found in this bounding box in previous frame,
+        // check which bounding boxes in the current frame also enclose is.
+        for (auto matchInPrev : matchesEnclosedInPrev)
+        {
+            int currKeyPointIdx = matchInPrev.trainIdx;
+            for (auto boundingBoxCurr : currFrame.boundingBoxes)
+            {
+                cv::Point currKeyPoint = currFrame.keypoints.at(currKeyPointIdx).pt;
+                if (boundingBoxCurr.roi.contains(currKeyPoint))
+                {
+                    sameMatchesCurr.insert({boundingBoxCurr.boxID, currKeyPointIdx});
+                }
+            }
+        }
+
+        // Check which bounding box in current frame has highest number of matches,
+        // then it will assigned as the best bounding box.
+        if (sameMatchesCurr.size() > 0)
+        {
+            int maxOccurance = 0;
+            int boundingBoxBestMatchedIdx;
+
+            for (auto sameMatchCurr : sameMatchesCurr)
+            {
+                int boxID = sameMatchCurr.first;
+                size_t boxIdOccurance = sameMatchesCurr.count(boxID);
+                if (boxIdOccurance > maxOccurance)
+                {
+                    maxOccurance = boxIdOccurance;
+                    boundingBoxBestMatchedIdx = boxID;
+                }
+            }
+            bbBestMatches.insert({boundingBoxPrev.boxID, boundingBoxBestMatchedIdx});
+        }
+    }
 }
